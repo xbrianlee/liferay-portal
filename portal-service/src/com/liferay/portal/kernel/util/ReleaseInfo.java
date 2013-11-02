84a6081337f513f69e04eedd663807b885eae102
Zsolt Balogh <zsolt.balogh@liferay.com>
2013-11-02 06:02:39 -0700

    Packager changes

--- portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ *
+ *
  */
 
 package com.liferay.portal.kernel.util;
@@ -144,7 +144,7 @@ public class ReleaseInfo {
 
 	private static final String _DATE = "November 1, 2013";
 
-	private static final String _NAME = "Liferay Portal Community Edition";
+	private static final String _NAME = "Liferay Portal Enterprise Edition";
 
 	private static final int _PARENT_BUILD_NUMBER = _BUILD_NUMBER;
 
