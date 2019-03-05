32ab416e04a1578eb610d13b31995364247bffb6
Brian Lee <brian.lee@liferay.com>
2019-03-05 09:14:10 -0800

    Revert "LPS-90958 Add sortable view count field for all asset entries"
    This reverts commit 1f47173490157878a2046bc85880b18c9c340d27.

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/contributor/document/AssetEntryDocumentContributor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/contributor/document/AssetEntryDocumentContributor.java
@@ -91,7 +91,6 @@ public class AssetEntryDocumentContributor implements DocumentContributor {
 		document.addLocalizedKeyword(
 			"localized_title",
 			_populateMap(assetEntry, assetEntry.getTitleMap()), true, true);
-		document.addNumber("viewCount", assetEntry.getViewCount());
 		document.addKeyword("visible", assetEntry.isVisible());
 	}
 
