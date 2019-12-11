57f168b6bede355573391e9b30b92df34032452e
Bryan Engler <bryan.engler@liferay.com>
2019-12-11 08:33:25 -0800

    LPS-102299 Update ElasticsearchFixture for unit tests. Ignore connection tests until more coverage is added in LPS-105312
    https://issues.liferay.com/browse/LPS-102299

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/connection/ElasticsearchFixture.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/connection/ElasticsearchFixture.java
@@ -149,6 +149,18 @@ public class ElasticsearchFixture implements ElasticsearchClientResolver {
 		return _embeddedElasticsearchConnection.getRestHighLevelClient();
 	}
 
+	@Override
+	public RestHighLevelClient getRestHighLevelClient(String connectionId) {
+		return _embeddedElasticsearchConnection.getRestHighLevelClient();
+	}
+
+	@Override
+	public RestHighLevelClient getRestHighLevelClient(
+		String connectionId, boolean preferLocalCluster) {
+
+		return _embeddedElasticsearchConnection.getRestHighLevelClient();
+	}
+
 	public void setClusterSettingsContext(
 		ClusterSettingsContext clusterSettingsContext) {
 
@@ -296,8 +308,6 @@ public class ElasticsearchFixture implements ElasticsearchClientResolver {
 
 		embeddedElasticsearchConnection.activate(
 			bundleContext, _elasticsearchConfigurationProperties);
-
-		embeddedElasticsearchConnection.connect();
 
 		return embeddedElasticsearchConnection;
 	}
