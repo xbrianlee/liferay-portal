067dc3267e7a80db50238ea3a2c58c25c504d9b0
Brian Lee <brian.lee@liferay.com>
2020-04-20 11:31:12 -0700

    Revert "LPS-111613 Returns the composite model if the permission support implementation is not found"
    This reverts commit 7a883d9c46d9fae7c5f4eb101259616b4573e9f4.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/security/permission/support/DDMPermissionSupportImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/security/permission/support/DDMPermissionSupportImpl.java
@@ -163,11 +163,6 @@ public class DDMPermissionSupportImpl implements DDMPermissionSupport {
 				_ddmPermissionSupportTracker.
 					getDDMStructurePermissionSupportServiceWrapper(className);
 
-		if (structurePermissionSupportServiceWrapper == null) {
-			return ResourceActionsUtil.getCompositeModelName(
-				className, DDMStructure.class.getName());
-		}
-
 		boolean defaultModelResourceName = MapUtil.getBoolean(
 			structurePermissionSupportServiceWrapper.getProperties(),
 			"default.model.resource.name");
