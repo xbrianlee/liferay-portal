d7651be050da949297c8660502fbf889d85df82e
Brian Lee <brian.lee@liferay.com>
2020-04-20 11:31:48 -0700

    Revert "LPS-111613 No longer need as ddmStructureLocalService.deleteStructure(dataDefinitionId) already removes the layouts"
    This reverts commit 55e4f4ef081d0ccd1c40ee869059aa739c6874ed.

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
@@ -139,6 +139,9 @@ public class DataDefinitionResourceImpl
 		_ddlRecordSetLocalService.deleteDDMStructureRecordSets(
 			dataDefinitionId);
 
+		DataLayoutResource dataLayoutResource = _getDataLayoutResource(false);
+
+		dataLayoutResource.deleteDataLayoutsDataDefinition(dataDefinitionId);
 
 		_ddmStructureLocalService.deleteStructure(dataDefinitionId);
 
