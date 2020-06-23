3045be7c98eae653466ed1c10e254b5c8f78d3ac
Bryan Engler <bryan.engler@liferay.com>
2020-06-23 12:47:59 -0700

    LPS-112607 portal-search-elasticsearch7-impl: Rename to SidecarManager and move to sidecar package

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
@@ -578,13 +578,13 @@ public class Sidecar {
 				}
 			}
 
-			SidecarComponentUtil.disableSidecarElasticsearchConnectionManager();
+			SidecarComponentUtil.disableSidecarManager();
 
 			if (_log.isInfoEnabled()) {
 				_log.info("Restarting sidecar Elasticsearch process");
 			}
 
-			SidecarComponentUtil.enableSidecarElasticsearchConnectionManager();
+			SidecarComponentUtil.enableSidecarManager();
 		}
 
 	}
