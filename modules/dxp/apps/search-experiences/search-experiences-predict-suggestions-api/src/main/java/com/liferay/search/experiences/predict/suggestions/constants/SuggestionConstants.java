1faf80b15aa11faadbb10dc1c14801b327cdf811
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-08-18 23:23:50 +0300

    LPS-134584 search-experiences-predict-suggestions-api: Constants reorganizing

--- modules/dxp/apps/search-experiences/search-experiences-predict-suggestions-api/src/main/java/com/liferay/search/experiences/predict/suggestions/constants/SuggestionConstants.java
+++ modules/dxp/apps/search-experiences/search-experiences-predict-suggestions-api/src/main/java/com/liferay/search/experiences/predict/suggestions/constants/SuggestionConstants.java
@@ -19,39 +19,15 @@ package com.liferay.search.experiences.predict.suggestions.constants;
  */
 public class SuggestionConstants {
 
-	public static final String DISPLAY_FIELD = "displayField";
-
-	public static final String ENTRY_CLASS_NAMES = "entryClassNames";
-
-	public static final String FIELD_MAP = "fieldMap";
-
 	public static final String FUZZINESS = "fuzziness";
 
-	public static final String HITCOUNT_THRESHOLD = "hitcountThreshold";
-
-	public static final String INDICES = "indices";
-
 	public static final String LANGUAGE_IDS = "languageIds";
-
-	public static final String NESTED_FIELD_MAP = "nestedFieldMap";
-
-	public static final String OFFSET = "offset";
 
 	public static final String OPERATOR = "operator";
 
 	public static final String PREFIX_LENGTH = "prefixLength";
 
-	public static final String SANITIZER_REGEXP = "sanitizerRegExp";
-
-	public static final String SORT_FIELD_MAP = "nestedFieldMap";
-
 	public static final String SOURCE_GROUP_IDS = "sourceGroupIds";
-
-	public static final String TERM_FILTERS = "termFilters";
-
-	public static final String TRIM_STOPWORDS = "trimStopWords";
-
-	public static final String TYPE = "type";
 
 	public static final String WEIGHT = "weight";
 
