3cfe601de60234217d0738c0230cdb6abac2dd4e
Bryan Engler <bryan.engler@liferay.com>
2022-11-21 12:28:03 -0800

    LPS-166987 LPS-168594 portal-workflow-metrics-rest-impl: Fix ds:unsatisfied warnings
    https://issues.liferay.com/browse/LPS-168594

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/resource/v1_0/IndexResourceImpl.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/resource/v1_0/IndexResourceImpl.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.engine.SearchEngineInformation;
 import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
 import com.liferay.portal.search.engine.adapter.index.RefreshIndexRequest;
 import com.liferay.portal.vulcan.pagination.Page;
@@ -76,6 +77,12 @@ public class IndexResourceImpl extends BaseIndexResourceImpl {
 
 	@Override
 	public void patchIndexRefresh(Index index) throws Exception {
+		if (Objects.equals(
+				_searchEngineInformation.getVendorString(), "Solr")) {
+
+			return;
+		}
+
 		if (Objects.isNull(index) || Validator.isNull(index.getKey())) {
 			throw new IndexKeyException();
 		}
@@ -243,7 +250,10 @@ public class IndexResourceImpl extends BaseIndexResourceImpl {
 	@Reference
 	private Language _language;
 
-	@Reference(target = "(search.engine.impl=Elasticsearch)")
-	private volatile SearchEngineAdapter _searchEngineAdapter;
+	@Reference
+	private SearchEngineAdapter _searchEngineAdapter;
+
+	@Reference
+	private SearchEngineInformation _searchEngineInformation;
 
 }
