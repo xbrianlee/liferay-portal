ec9edd141e89fa1f91277acf6bb2cbe15c445bc9
Tibor Lipusz <tibor.lipusz@liferay.com>
2023-02-15 12:45:06 +0100

    LPS-176393 LPS-183776 portal-search-tuning: Use mappings & settings API
    https://issues.liferay.com/browse/LPS-183776

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexCreatorImpl.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexCreatorImpl.java
@@ -14,6 +14,11 @@
 
 package com.liferay.portal.search.tuning.rankings.web.internal.index;
 
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
 import com.liferay.portal.search.engine.adapter.index.CreateIndexRequest;
@@ -26,24 +31,27 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Wade Cao
  * @author Adam Brandizzi
+ * @author Joshua Cords
+ * @author Tibor Lipusz
  */
 @Component(service = RankingIndexCreator.class)
 public class RankingIndexCreatorImpl implements RankingIndexCreator {
 
 	@Override
 	public void create(RankingIndexName rankingIndexName) {
-		String mappings = StringUtil.read(
-			getClass(), _INDEX_MAPPINGS_RESOURCE_NAME);
-
-		String source = StringUtil.read(
-			getClass(), _INDEX_SETTINGS_RESOURCE_NAME);
-
 		CreateIndexRequest createIndexRequest = new CreateIndexRequest(
 			rankingIndexName.getIndexName());
 
-		createIndexRequest.setMappings(mappings);
+		try {
+			createIndexRequest.setMappings(
+				_readJSONResource(_INDEX_MAPPINGS_RESOURCE_NAME));
 
-		createIndexRequest.setSource(source);
+			createIndexRequest.setSettings(
+				_readJSONResource(_INDEX_SETTINGS_RESOURCE_NAME));
+		}
+		catch (JSONException jsonException) {
+			_log.error(jsonException);
+		}
 
 		_searchEngineAdapter.execute(createIndexRequest);
 	}
@@ -56,11 +64,24 @@ public class RankingIndexCreatorImpl implements RankingIndexCreator {
 		_searchEngineAdapter.execute(deleteIndexRequest);
 	}
 
+	private String _readJSONResource(String fileName) throws JSONException {
+		JSONObject jsonObject = _jsonFactory.createJSONObject(
+			StringUtil.read(getClass(), fileName));
+
+		return jsonObject.toString();
+	}
+
 	private static final String _INDEX_MAPPINGS_RESOURCE_NAME =
 		"/META-INF/search/liferay-search-tuning-rankings-index-mappings.json";
 
 	private static final String _INDEX_SETTINGS_RESOURCE_NAME =
 		"/META-INF/search/liferay-search-tuning-rankings-index-settings.json";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RankingIndexCreatorImpl.class);
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference
 	private SearchEngineAdapter _searchEngineAdapter;
