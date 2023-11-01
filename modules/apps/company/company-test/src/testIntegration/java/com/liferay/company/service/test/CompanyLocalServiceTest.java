bc91f904767b9d3c8a579437dd16bfffcaf690d8
Lucas Falcao <lucas.falcao@liferay.com>
2023-11-01 11:33:10 -0300

    Revert "LPS-196393 Tests: add a check anytime we create a company to be sure it is random and we do not use the counter"
    This reverts commit dc11a752fe1359a757429b6d1dd3cbffd89014c6.

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
@@ -7,7 +7,6 @@ package com.liferay.company.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.link.model.adapter.StagedAssetLink;
-import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.document.library.kernel.model.DLFileEntryMetadata;
 import com.liferay.document.library.kernel.model.DLFileEntryType;
 import com.liferay.document.library.kernel.model.DLFileEntryTypeConstants;
@@ -78,7 +77,6 @@ import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -972,17 +970,7 @@ public class CompanyLocalServiceTest {
 	}
 
 	protected Company addCompany() throws Exception {
-		long counterCompanyId = _counterLocalService.increment() + 1;
-
-		long[] companyIds = PortalInstances.getCompanyIds();
-
-		Company company = addCompany(
-			RandomTestUtil.randomString() + "test.com");
-
-		_verifyRandomCompanyId(
-			company.getCompanyId(), counterCompanyId, companyIds);
-
-		return company;
+		return addCompany(RandomTestUtil.randomString() + "test.com");
 	}
 
 	protected Company addCompany(String webId) throws Exception {
@@ -1206,16 +1194,6 @@ public class CompanyLocalServiceTest {
 		return list;
 	}
 
-	private void _verifyRandomCompanyId(
-		long companyId, long counterCompanyId, long[] companyIds) {
-
-		Assert.assertFalse(ArrayUtil.contains(companyIds, companyId));
-
-		Assert.assertNotEquals(counterCompanyId, companyId);
-
-		Assert.assertTrue(companyId > 0);
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		CompanyLocalServiceTest.class);
 
@@ -1238,9 +1216,6 @@ public class CompanyLocalServiceTest {
 
 	@Inject
 	private CompanyLocalService _companyLocalService;
-
-	@Inject
-	private CounterLocalService _counterLocalService;
 
 	@Inject
 	private DDMStructureLocalService _ddmStructureLocalService;
